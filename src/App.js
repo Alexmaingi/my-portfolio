62c55a79744f1d869205e01c665289f23a28124f
Alexmaingi <alexmaingi077@gmail.com>
2023-03-03 04:43:33 +0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-2xl font-bold flex justify-center items-center top-0 left-0 w-full h-screen bg-black text-green-700">
+      Hello I am Alex Maingi a Software Developer and welcome to my portfolio
     </div>
   );
 }
